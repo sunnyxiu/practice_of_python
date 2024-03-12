40c19516b534a3f37d9b4dc5de9ae074d75cbdac
sunnyxiu <61191922+sunnyxiu@users.noreply.github.com>
2024-03-12 08:01:50 +0800

    Update 8.4 factors.py

--- 8.4 factors.py
+++ 8.4 factors.py
@@ -3,6 +3,6 @@ integer = input("Enter a positive integer: ")
 if int(integer) <= 0:
     print(f"{integer} is not a positive integer.")
 else:
-    for i in range(int(integer)):
-        if int(integer) % (i + 1) == 0: #(i+1) range from 1 to 12
-            print(f"{i+1} is a factor of {integer}")
+    for i in range(1, int(integer)+1):
+        if int(integer) % i == 0: 
+            print(f"{i} is a factor of {integer}")
